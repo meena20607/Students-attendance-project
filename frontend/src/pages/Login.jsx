4bc6c4cc69ef3c33f7472dcb257e3cdbf13bf613
Meena <20607.meena@gmail.com>
2026-06-16 22:21:38 +0530

    Frontend structure setup

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+function Login() {
+  return (
+    <div>
+      <h1>Student Attendance Management System</h1>
+
+      <h2>Login</h2>
+
+      <input type="email" placeholder="Email" />
+      <br /><br />
+
+      <input type="password" placeholder="Password" />
+      <br /><br />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
